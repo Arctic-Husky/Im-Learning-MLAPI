3ad31011806475b21a5e16027890f11f5ceae69a
Caio Schmidt <caiofonseca6@gmail.com>
2021-04-06 21:04:21 -0300

    Android Build
    Just a test multiplayer game with Mirror

---
+++ Mirror Multiplayer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+using DitzeGames.MobileJoystick;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private Joystick joystick;
+
+    private CharacterController characterController;
+    private Vector3 movimento;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+    void Update()
+    {
+        if(!isLocalPlayer)
+            return;
+
+        /*movX = Input.GetAxis("Horizontal");
+        movZ = Input.GetAxis("Vertical");*/
+
+        //movimento = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movimento = new Vector3(joystick.AxisNormalized.x, 0, joystick.AxisNormalized.y);
+        movimento = Vector3.ClampMagnitude(movimento, 1f);
+        movimento = transform.TransformDirection(movimento);
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+            return;
+        characterController.SimpleMove(movimento * speed);
+    }
+}
